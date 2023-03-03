2495bbde4e342671e8fd3d538ec22d22e51674db
yodaum <yod.aum@gmail.com>
2023-03-03 04:53:37 +0000

    Completed Queue Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Queue;
 
@@ -36,7 +37,9 @@ public class Lab {
      * Stack behavior.
      */
     public Deque<String> createQueue(){
-        return null;
+        //Create ArrayDeque  object.
+		Deque newDeque  = new ArrayDeque();
+        return newDeque;
     }
 
     /**
@@ -45,7 +48,7 @@ public class Lab {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        return queue.size();
     }
 
     /**
@@ -56,6 +59,8 @@ public class Lab {
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
 
+        queue.add(item);
+
     }
 
     /**
@@ -65,7 +70,7 @@ public class Lab {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+        return queue.poll();
     }
 
     /**
@@ -75,6 +80,6 @@ public class Lab {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+        return queue.peek();
     }
 }
